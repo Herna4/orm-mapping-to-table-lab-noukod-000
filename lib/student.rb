8cfa81494fa9390fe99e747b4af3264769f45eb3
Herna Michel <labellehernamichel@yahoo.fr>
2019-05-18 05:48:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -25,5 +25,21 @@ class Student
   end
   
   def self.drop_table 
+    # def save
+    # sql = <<-SQL
+    #   INSERT INTO students (name, grade)
+    #     VALUES (?, ?)
+    # SQL
+
+    # DB[:conn].execute(sql, self.name, self.grade)
+    # end
+
+   def self.create(name:, grade:)
+    student = Student.new(name, grade)
+    student.save
+    student
+  end
+
+
   end
 end
